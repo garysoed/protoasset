8672fd70a0790c9939840a2817f12c58b8be3419
Gary Soedarsono <garysoed@gmail.com>
2017-08-20 10:48:08 -0700

    Improved SettingsView

--- src/project/settings-view.ts
+++ src/project/settings-view.ts
@@ -1,92 +1,116 @@
+import { DataAccess } from 'external/gs_tools/src/datamodel';
+import { monad, monadOut, on } from 'external/gs_tools/src/event';
 import { ImmutableSet } from 'external/gs_tools/src/immutable';
 import { inject } from 'external/gs_tools/src/inject';
-import { StringParser } from 'external/gs_tools/src/parse';
-import { customElement, DomHook, handle, hook } from 'external/gs_tools/src/webc';
-import { RouteService } from 'external/gs_ui/src/routing';
+import { MonadSetter, MonadValue } from 'external/gs_tools/src/interfaces';
+import { DataModelParser, StringParser } from 'external/gs_tools/src/parse';
+import { customElement, dom, domOut, onDom, onLifecycle } from 'external/gs_tools/src/webc';
 
-import { BaseThemedElement } from 'external/gs_ui/src/common';
+import { BaseThemedElement2 } from 'external/gs_ui/src/common';
 import { RouteServiceEvents } from 'external/gs_ui/src/const';
+import { RouteNavigator, RouteService } from 'external/gs_ui/src/routing';
 import { ThemeService } from 'external/gs_ui/src/theming';
+import { DownloadService } from 'external/gs_ui/src/tool';
 
-import { FuseBackedManager } from '../data/fuse-backed-manager';
-import { Project, ProjectSearchIndex } from '../data/project';
+import { Project } from '../data/project';
 import { ProjectManager } from '../data/project-manager';
 import { Editor } from '../project/editor';
 import { RouteFactoryService } from '../routing/route-factory-service';
 import { Views } from '../routing/views';
 
-type ProjectManagerType = FuseBackedManager<ProjectSearchIndex, Project>;
+const DOWNLOAD_EL = '#downloadButton';
+const EDITOR_EL = '#editor';
+const NAME_EL = '#name';
+const EDITOR_NAME_ATTR = {name: 'project-name', parser: StringParser, selector: EDITOR_EL};
+const NAME_INNER_TEXT = {parser: StringParser, selector: NAME_EL};
 
 /**
  * Settings
  */
 @customElement({
-  dependencies: ImmutableSet.of([Editor, RouteService]),
+  dependencies: ImmutableSet.of([DownloadService, Editor, RouteService]),
   tag: 'pa-project-settings-view',
   templateKey: 'src/project/settings-view',
 })
-export class SettingsView extends BaseThemedElement {
-  @hook('#editor').attribute('project-name', StringParser)
-  readonly editorProjectNameHook_: DomHook<string> = DomHook.of<string>();
-
-  @hook('#name').innerText()
-  readonly nameInnerTextHook_: DomHook<string> = DomHook.of<string>();
-
-  private readonly projectManager_: ProjectManagerType = ProjectManager;
-
+export class SettingsView extends BaseThemedElement2 {
   constructor(
+      @inject('gs.tool.DownloadService') private readonly downloadService_: DownloadService,
       @inject('pa.routing.RouteFactoryService')
-      private readonly routeFactoryService_: RouteFactoryService,
+          private readonly routeFactoryService_: RouteFactoryService,
       @inject('gs.routing.RouteService')
-      private readonly routeService_: RouteService<Views>,
-      @inject('theming.ThemeService')
-      themeService: ThemeService) {
+          private readonly routeService_: RouteService<Views>,
+      @inject('theming.ThemeService') themeService: ThemeService) {
     super(themeService);
   }
 
-  private async getProject_(): Promise<Project | null> {
-    const route = this.routeService_.monad()
-        .get().getRoute(this.routeFactoryService_.projectSettings());
+  private async getProject_(
+      routeNavigator: RouteNavigator<Views>,
+      projectAccess: DataAccess<Project>): Promise<Project | null> {
+    const route = routeNavigator.getRoute(this.routeFactoryService_.projectSettings());
     if (route === null) {
       return null;
     }
 
-    return this.projectManager_.monad().get().get(route.params.projectId);
+    return projectAccess.get(route.params.projectId);
   }
 
-  onCreated(element: HTMLElement): void {
-    super.onCreated(element);
-    this.addDisposable(
-        this.routeService_.on(RouteServiceEvents.CHANGED, this.onRouteChanged_, this));
-    this.onRouteChanged_();
-  }
-
-  @handle('#editor').attributeChange('project-name')
-  async onEditorProjectNameChanged_(): Promise<void> {
-    const project = await this.getProject_();
-    if (project === null) {
+  @onDom.event(DOWNLOAD_EL, 'click')
+  async onDownloadClick_(
+      @monad((view: SettingsView) => view.routeService_.monad())
+          routeNavigator: RouteNavigator<Views>,
+      @monad(ProjectManager.monad()) projectAccess: DataAccess<Project>): Promise<void> {
+    const project = await this.getProject_(routeNavigator, projectAccess);
+    if (!project) {
       return;
     }
 
-    const projectName = this.editorProjectNameHook_.get();
+    this.downloadService_.downloadString(
+        DataModelParser<Project>().stringify(project),
+        `${project.getName()}.pa`);
+    // TODO: Implement
+    throw new Error('Unimplemented');
+  }
+
+  @onDom.attributeChange(EDITOR_NAME_ATTR)
+  async onEditorProjectNameChanged_(
+      @dom.attribute(EDITOR_NAME_ATTR) projectName: string | null,
+      @monad((view: SettingsView) => view.routeService_.monad())
+          routeNavigator: RouteNavigator<Views>,
+      @monadOut(ProjectManager.monad()) projectSetter: MonadSetter<DataAccess<Project>>):
+      Promise<MonadValue<any>[]> {
     if (projectName === null) {
-      return;
+      return [];
     }
 
-    const projectManagerMonad = this.projectManager_.monad();
-    projectManagerMonad.set(
-        projectManagerMonad.get().queueUpdate(project.getId(), project.setName(projectName)));
+    const project = await this.getProject_(routeNavigator, projectSetter.value);
+    if (project === null) {
+      return [];
+    }
+
+    return [
+      projectSetter.set(projectSetter.value.queueUpdate(
+          project.getId(), project.setName(projectName))),
+    ];
   }
 
-  private async onRouteChanged_(): Promise<void> {
-    const project = await this.getProject_();
+  @onLifecycle('create')
+  @on((view: SettingsView) => view.routeService_, RouteServiceEvents.CHANGED)
+  async onRouteChanged_(
+      @monad((view: SettingsView) => view.routeService_.monad())
+          routeNavigator: RouteNavigator<Views>,
+      @monad(ProjectManager.monad()) projectAccess: DataAccess<Project>,
+      @domOut.attribute(EDITOR_NAME_ATTR) editorNameSetter: MonadSetter<string | null>,
+      @domOut.innerText(NAME_INNER_TEXT) nameSetter: MonadSetter<string | null>):
+      Promise<MonadValue<any>[]> {
+    const project = await this.getProject_(routeNavigator, projectAccess);
     if (project === null) {
-      return;
+      return [];
     }
 
     const projectName = project.getName();
-    this.nameInnerTextHook_.set(projectName);
-    this.editorProjectNameHook_.set(projectName);
+    return [
+      editorNameSetter.set(projectName),
+      nameSetter.set(projectName),
+    ];
   }
 }
-// TODO: Mutable
--- src/project/settings-view_test.ts
+++ src/project/settings-view_test.ts
@@ -1,149 +1,147 @@
-import { assert, Matchers, TestBase } from '../test-base';
+import { assert, TestBase } from '../test-base';
 TestBase.setup();
 
 import { DataAccess, FakeDataAccess } from 'external/gs_tools/src/datamodel';
+import { FakeMonadSetter } from 'external/gs_tools/src/event';
 import { ImmutableMap } from 'external/gs_tools/src/immutable';
 import { Mocks } from 'external/gs_tools/src/mock';
 import { TestDispose } from 'external/gs_tools/src/testing';
 
-import { RouteServiceEvents } from 'external/gs_ui/src/const';
+import { FakeRouteNavigator } from 'external/gs_ui/src/routing';
 
 import { Project } from '../data/project';
-import { ProjectManager } from '../data/project-manager';
 import { SettingsView } from '../project/settings-view';
+import { TestRouteFactoryService } from '../routing/test-route-factory-service';
+import { Views } from '../routing/views';
 
 describe('project.SettingsView', () => {
-  let mockRouteFactoryService: any;
-  let mockRouteService: any;
+  let mockDownloadService: any;
   let view: SettingsView;
 
   beforeEach(() => {
-    mockRouteFactoryService = jasmine.createSpyObj('RouteFactoryService', ['projectSettings']);
-    mockRouteService = jasmine.createSpyObj('RouteService', ['getParams', 'on']);
+    mockDownloadService = jasmine.createSpyObj('DownloadService', ['downloadString']);
     view = new SettingsView(
-        mockRouteFactoryService,
-        mockRouteService,
+        mockDownloadService,
+        TestRouteFactoryService,
+        Mocks.object('RouteService'),
         jasmine.createSpyObj('ThemeService', ['applyTheme']));
     TestDispose.add(view);
   });
 
-  xdescribe('getProject_', () => {
+  describe('getProject_', () => {
     it('should resolve the correct project', async () => {
-      const project = Mocks.object('project');
       const projectId = 'projectId';
-      mockRouteService.getParams.and.returnValue({projectId});
+      const project = Project.withId(projectId);
 
       const projectDataAccess = new FakeDataAccess(
         ImmutableMap.of([[projectId, project]]),
       );
-      const mockProjectManagerMonad = jasmine.createSpyObj('ProjectManagerMonad', ['get']);
-      mockProjectManagerMonad.get.and.returnValue(projectDataAccess);
-      spyOn(ProjectManager, 'monad').and.returnValue(() => mockProjectManagerMonad);
 
-      const routeFactory = Mocks.object('routeFactory');
-      mockRouteFactoryService.projectSettings.and.returnValue(routeFactory);
+      const fakeRouteNavigator = new FakeRouteNavigator<Views>();
+      spyOn(fakeRouteNavigator, 'getRoute').and.returnValue({params: {projectId}});
 
-      assert(await view['getProject_']()).to.equal(project);
-      assert(mockRouteService.getParams).to.haveBeenCalledWith(routeFactory);
+      assert(await view['getProject_'](fakeRouteNavigator, projectDataAccess)).to.equal(project);
+      assert(fakeRouteNavigator.getRoute).to
+          .haveBeenCalledWith(TestRouteFactoryService.projectSettings());
     });
 
-    it('should resolve with null if params cannot be determined', async () => {
-      mockRouteService.getParams.and.returnValue(null);
+    it('should resolve with null if route cannot be determined', async () => {
+      const projectDataAccess = new FakeDataAccess<Project>();
 
-      const routeFactory = Mocks.object('routeFactory');
-      mockRouteFactoryService.projectSettings.and.returnValue(routeFactory);
+      const fakeRouteNavigator = new FakeRouteNavigator<Views>();
+      spyOn(fakeRouteNavigator, 'getRoute').and.returnValue(null);
 
-      assert(await view['getProject_']()).to.equal(null);
-      assert(mockRouteService.getParams).to.haveBeenCalledWith(routeFactory);
-    });
-  });
-
-  describe('onCreated', () => {
-    it('should initialize correctly', () => {
-      spyOn(view, 'onRouteChanged_');
-      spyOn(view, 'listenTo');
-      spyOn(view, 'addDisposable').and.callThrough();
-
-      const mockDisposable = jasmine.createSpyObj('Disposable', ['dispose']);
-      mockRouteService.on.and.returnValue(mockDisposable);
-
-      view.onCreated(Mocks.object('element'));
-      assert(view['onRouteChanged_']).to.haveBeenCalledWith();
-      assert(view.addDisposable).to.haveBeenCalledWith(mockDisposable);
-      assert(mockRouteService.on).to.haveBeenCalledWith(
-          RouteServiceEvents.CHANGED,
-          view['onRouteChanged_'],
-          view);
+      assert(await view['getProject_'](fakeRouteNavigator, projectDataAccess)).to.beNull();
+      assert(fakeRouteNavigator.getRoute).to
+          .haveBeenCalledWith(TestRouteFactoryService.projectSettings());
     });
   });
 
-  xdescribe('onEditorProjectNameChanged_', () => {
+  describe('onEditorProjectNameChanged_', () => {
     it('should update the project correctly', async () => {
       const projectId = 'projectId';
       const mockProject = Project.withId(projectId);
       spyOn(view, 'getProject_').and.returnValue(Promise.resolve(mockProject));
 
       const projectName = 'projectName';
-      spyOn(view.editorProjectNameHook_, 'get').and.returnValue(projectName);
+      const projectDataAccess = new FakeDataAccess<Project>();
+      const fakeRouteNavigator = new FakeRouteNavigator<Views>();
+      const fakeProjectSetter = new FakeMonadSetter<DataAccess<Project>>(projectDataAccess);
 
-      const projectDataAccess = new FakeDataAccess();
-      const mockProjectManagerMonad = jasmine.createSpyObj('ProjectManagerMonad', ['get', 'set']);
-      mockProjectManagerMonad.get.and.returnValue(projectDataAccess);
-      spyOn(ProjectManager, 'monad').and.returnValue(() => mockProjectManagerMonad);
-
-      await view.onEditorProjectNameChanged_();
-      assert(mockProjectManagerMonad.set).to.haveBeenCalledWith(Matchers.any(DataAccess));
-      const dataAccess: DataAccess<Project> = mockProjectManagerMonad.set.calls.argsFor(0)[0];
-      assert(dataAccess.getUpdateQueue().get(projectId)!.getName()).to.equal(projectName);
-    });
-
-    it('should do nothing if the editor project name is null', async () => {
-      const mockProject = jasmine.createSpyObj('Project', ['setName']);
-      spyOn(view, 'getProject_').and.returnValue(Promise.resolve(mockProject));
-
-      spyOn(view.editorProjectNameHook_, 'get').and.returnValue(null);
-
-      const mockProjectManagerMonad = jasmine.createSpyObj('ProjectManagerMonad', ['get', 'set']);
-      spyOn(ProjectManager, 'monad').and.returnValue(() => mockProjectManagerMonad);
-
-      await view.onEditorProjectNameChanged_();
-      assert(mockProjectManagerMonad.set).toNot.haveBeenCalled();
-      assert(mockProject.setName).toNot.haveBeenCalled();
+      const updates = await view.onEditorProjectNameChanged_(
+          projectName,
+          fakeRouteNavigator,
+          fakeProjectSetter);
+      const newProject = fakeProjectSetter
+          .findValue(updates)!.value.getUpdateQueue().get(projectId)!;
+      assert(newProject.getName()).to.equal(projectName);
+      assert(view['getProject_']).to.haveBeenCalledWith(fakeRouteNavigator, projectDataAccess);
     });
 
     it('should do nothing if the project cannot be found', async () => {
       spyOn(view, 'getProject_').and.returnValue(Promise.resolve(null));
 
-      const mockProjectManagerMonad = jasmine.createSpyObj('ProjectManagerMonad', ['get', 'set']);
-      spyOn(ProjectManager, 'monad').and.returnValue(() => mockProjectManagerMonad);
+      const projectName = 'projectName';
+      const projectDataAccess = new FakeDataAccess<Project>();
+      const fakeRouteNavigator = new FakeRouteNavigator<Views>();
+      const fakeProjectSetter = new FakeMonadSetter<DataAccess<Project>>(projectDataAccess);
 
-      await view.onEditorProjectNameChanged_();
-      assert(mockProjectManagerMonad.set).toNot.haveBeenCalled();
+      const updates = await view.onEditorProjectNameChanged_(
+          projectName,
+          fakeRouteNavigator,
+          fakeProjectSetter);
+      assert(updates).to.equal([]);
+    });
+
+    it('should do nothing if the editor project name is null', async () => {
+      const projectName = 'projectName';
+      const projectDataAccess = new FakeDataAccess<Project>();
+      const fakeRouteNavigator = new FakeRouteNavigator<Views>();
+      const fakeProjectSetter = new FakeMonadSetter<DataAccess<Project>>(projectDataAccess);
+
+      const updates = await view.onEditorProjectNameChanged_(
+          projectName,
+          fakeRouteNavigator,
+          fakeProjectSetter);
+      assert(updates).to.equal([]);
     });
   });
 
   describe('onRouteChanged_', () => {
     it('should update the UI correctly', async () => {
       const projectName = 'projectName';
-      const mockProject = jasmine.createSpyObj('Project', ['getName']);
-      mockProject.getName.and.returnValue(projectName);
-      spyOn(view, 'getProject_').and.returnValue(Promise.resolve(mockProject));
-      spyOn(view.editorProjectNameHook_, 'set');
-      spyOn(view.nameInnerTextHook_, 'set');
+      const project = Project.withId('projectId').setName(projectName);
+      spyOn(view, 'getProject_').and.returnValue(Promise.resolve(project));
 
-      await view['onRouteChanged_']();
-      assert(view.editorProjectNameHook_.set).to.haveBeenCalledWith(projectName);
-      assert(view.nameInnerTextHook_.set).to.haveBeenCalledWith(projectName);
+      const projectAccess = new FakeDataAccess<Project>();
+      const fakeEditorNameSetter = new FakeMonadSetter<string | null>(null);
+      const fakeNameSetter = new FakeMonadSetter<string | null>(null);
+      const fakeRouteNavigator = new FakeRouteNavigator<Views>();
+
+      const updates = await view.onRouteChanged_(
+          fakeRouteNavigator,
+          projectAccess,
+          fakeEditorNameSetter,
+          fakeNameSetter);
+      assert(fakeEditorNameSetter.findValue(updates)!.value).to.equal(projectName);
+      assert(fakeNameSetter.findValue(updates)!.value).to.equal(projectName);
+      assert(view['getProject_']).to.haveBeenCalledWith(fakeRouteNavigator, projectAccess);
     });
 
     it('should do nothing if project cannot be found', async () => {
       spyOn(view, 'getProject_').and.returnValue(Promise.resolve(null));
-      spyOn(view.editorProjectNameHook_, 'set');
-      spyOn(view.nameInnerTextHook_, 'set');
 
-      await view['onRouteChanged_']();
-      assert(view.editorProjectNameHook_.set).toNot.haveBeenCalled();
-      assert(view.nameInnerTextHook_.set).toNot.haveBeenCalled();
+      const projectAccess = new FakeDataAccess<Project>();
+      const fakeEditorNameSetter = new FakeMonadSetter<string | null>(null);
+      const fakeNameSetter = new FakeMonadSetter<string | null>(null);
+      const fakeRouteNavigator = new FakeRouteNavigator<Views>();
+
+      const updates = await view.onRouteChanged_(
+          fakeRouteNavigator,
+          projectAccess,
+          fakeEditorNameSetter,
+          fakeNameSetter);
+      assert(updates).to.equal([]);
     });
   });
 });
